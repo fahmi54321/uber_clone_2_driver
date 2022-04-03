cb711af714dfdaf3d4d2f87f068b8dc1793a6555
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-04-03 14:32:54 +0700

    setting up tabcontroller and tabpages

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,30 +1,73 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:uber_clone_2_driver/brand_colors.dart';
+import 'package:uber_clone_2_driver/tabs/earningstab.dart';
+import 'package:uber_clone_2_driver/tabs/hometab.dart';
+import 'package:uber_clone_2_driver/tabs/profiletab.dart';
+import 'package:uber_clone_2_driver/tabs/ratingstab.dart';
 
 class MainPage extends StatefulWidget {
-
   static const String id = 'mainpage';
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+
+  TabController tabController; //todo 1
+  int selectedIndex = 0; //todo 2
+
+  void onItemClicked(int index){ //todo 3
+    setState(() {
+      selectedIndex = index;
+      tabController.index = selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 4, vsync: this); //todo 4
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    tabController.dispose(); //todo 5
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Main page'),),
-      body: Center(child: MaterialButton(
-        onPressed: (){
-
-          DatabaseReference ref = FirebaseDatabase.instance.reference().child('testing');
-          ref.set('testing connection');
-
-        },
-        color: Colors.blue,
-        minWidth: 200,
-        child: Text('Hello'),
-      ),),
+      body: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: [
+          HomeTab(),
+          EarningsTab(),
+          RatingsTab(),
+          ProfileTab(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items:<BottomNavigationBarItem> [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: 'Earnings'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Ratings'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+        ],
+        unselectedItemColor: BrandColors.colorIcon,
+        selectedItemColor: BrandColors.colorOrange,
+        showSelectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        type: BottomNavigationBarType.fixed,
+        onTap: onItemClicked, //todo 6
+        currentIndex: selectedIndex, //todo 7 (finish)
+      ),
     );
   }
 }
---
+++ lib/tabs/earningstab.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EarningsTab extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Hello Earnings tab'),);
+  }
+}
