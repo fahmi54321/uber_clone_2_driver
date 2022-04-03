a1b4f79bd3f341ec6c67c22939cdb5b33537707a
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-04-03 07:22:12 +0700

    setting up firebase packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uber_clone_2_driver/screens/mainpage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'db2',
+    options: Platform.isIOS
+        ? const FirebaseOptions(
+      googleAppID: '1:448216127030:android:86b118e2051acfc662aa97',
+      gcmSenderID: '297855924061',
+      databaseURL: 'https://uber-d5e16-default-rtdb.firebaseio.com',
+    )
+        : const FirebaseOptions(
+      googleAppID: '1:448216127030:android:86b118e2051acfc662aa97',
+      apiKey: 'AIzaSyD12_TVFRT2F_k-QMrfFt7vUpYhITwoHYQ',
+      databaseURL: 'https://uber-d5e16-default-rtdb.firebaseio.com',
+    ),
+  );
   runApp(MyApp());
 }
 
--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -13,6 +14,17 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Main page'),),
+      body: Center(child: MaterialButton(
+        onPressed: (){
+
+          DatabaseReference ref = FirebaseDatabase.instance.reference().child('testing');
+          ref.set('testing connection');
+
+        },
+        color: Colors.blue,
+        minWidth: 200,
+        child: Text('Hello'),
+      ),),
     );
   }
 }
