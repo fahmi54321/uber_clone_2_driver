ceff24674bf6b7fc7730fe29818fb96aaa8f217e
fahmi54321 <fahmiabdulaziz44@gmail.com>
2022-04-15 13:34:21 +0700

    reading data notification messages

--- lib/helpers/pushnotificationservice.dart
+++ lib/helpers/pushnotificationservice.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:uber_clone_2_driver/globalvariabel.dart';
@@ -10,12 +12,31 @@ class PushNotificationService{
     fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
         print("onMessage: $message");
+
+        //todo 1
+        if(Platform.isAndroid){
+          String rideID = message['data']['ride_id'];
+          print('ride_id : $rideID');
+        }
+
       },
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
+
+        //todo 2
+        if(Platform.isAndroid){
+          String rideID = message['data']['ride_id'];
+          print('ride_id : $rideID');
+        }
       },
       onResume: (Map<String, dynamic> message) async {
         print("onResume: $message");
+
+        //todo 3 (finish)
+        if(Platform.isAndroid){
+          String rideID = message['data']['ride_id'];
+          print('ride_id : $rideID');
+        }
       },
     );
   }
@@ -33,13 +54,3 @@ class PushNotificationService{
   }
 
 }
-
-//todo 1 (finish)
-/**
- * Testing dengan postman
- * 'Content-Type': 'application/json',
- * 'Authorization': 'key=AAAAaFvDVjY:APA91bHMNMeFRHcJgO7DunLEkQXpAl3qxUSkeIxs5qjKFWB-0NAk7T0-0o4ac7EIRLEYdvgSgWJyVqR6xgytp3mnHF4kEQe3OJ_qzxBpxDIYBjtr9a5cp_WbeOWbc5fnyggrOI2x2_m9',
- *  url : https://fcm.googleapis.com/fcm/send
- *  body : (raw)
- *  {"notification": {"body": "this is a body","title": "this is a title"}, "priority": "high", "data": {"click_action": "FLUTTER_NOTIFICATION_CLICK", "id": "1", "status": "done"}, "to": "<FCM TOKEN>"}
- */
